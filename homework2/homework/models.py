f8db542e09f878f5f5cef7a971f927b1865b6867
thisisanu <168681048+thisisanu@users.noreply.github.com>
2025-10-25 00:06:58 -0700

    Update models.py

--- homework2/homework/models.py
+++ homework2/homework/models.py
@@ -172,7 +172,7 @@ class MLPClassifierDeepResidual(nn.Module):
         h: int = 64,
         w: int = 64,
         num_classes: int = 6,
-        hidden_dim: int = 128,
+        hidden_dim: int = 256,
         num_layers: int = 5,
         dropout: float = 0.1
     ):
