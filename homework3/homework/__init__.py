f6d0efc17fecaee2bd1ef575b7f502f85a061c7c
thisisanu <168681048+thisisanu@users.noreply.github.com>
2025-11-09 23:32:42 -0800

    Remove load_model import from package init
    Removed import of load_model from __init__.py.

--- homework3/homework/__init__.py
+++ homework3/homework/__init__.py
@@ -1,4 +1,3 @@
 # Make homework a package
 # Optional: expose commonly used modules at package level
-from .models import load_model
 from .datasets import SuperTuxClassificationDataset, load_data, get_class_names
