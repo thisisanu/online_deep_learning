a6e3d2a0a7769bbc0c9f5237c87fb75e612a5a36
thisisanu <168681048+thisisanu@users.noreply.github.com>
2025-11-10 01:14:15 -0800

    Update __init__.py

--- homework3/homework/datasets/__init__.py
+++ homework3/homework/datasets/__init__.py
@@ -1,3 +1,3 @@
 # Make datasets a subpackage
-# Optional: expose classes/functions from classification_dataset
 from .classification_dataset import SuperTuxClassificationDataset, load_data, get_class_names
+from .road_dataset import load_data
