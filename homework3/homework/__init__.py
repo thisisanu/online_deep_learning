b1ebd783a68a87ce17d15a98b0f5873e02dddf57
thisisanu <168681048+thisisanu@users.noreply.github.com>
2025-11-10 00:20:49 -0800

    Update __init__.py

--- homework3/homework/__init__.py
+++ homework3/homework/__init__.py
@@ -1,3 +1,4 @@
 # Make homework a package
 # Optional: expose commonly used modules at package level
+from .models import load_model, save_model, Classifier
 from .datasets import SuperTuxClassificationDataset, load_data, get_class_names
