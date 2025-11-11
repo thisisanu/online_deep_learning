e47ac5f164b45fb9a1a2c4817ec0c8c62627a5e1
thisisanu <168681048+thisisanu@users.noreply.github.com>
2025-11-10 21:06:56 -0800

    Update __init__.py

--- homework3/homework/__init__.py
+++ homework3/homework/__init__.py
@@ -3,8 +3,7 @@
 from .models import load_model, save_model, Classifier
 from .datasets import SuperTuxClassificationDataset, load_data, get_class_names
 from .datasets.road_dataset import load_data
-# Import RandomBrightnessContrast from road_transforms
-from .road_transforms import RandomBrightnessContrast
+from .datasets.road_transforms import RandomBrightnessContrast
 # Expose it when importing the datasets package
 __all__ = ["RandomBrightnessContrast"]
 
