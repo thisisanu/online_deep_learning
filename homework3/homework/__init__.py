ca3b482c88242c0c9b84cb062c6de46b206428cf
thisisanu <168681048+thisisanu@users.noreply.github.com>
2025-12-08 21:57:07 -0800

    Remove duplicate imports in __init__.py

--- homework3/homework/__init__.py
+++ homework3/homework/__init__.py
@@ -1,2 +1,4 @@
 from .datasets.classification_dataset import SuperTuxClassificationDataset, get_class_names, load_data
+from .classification_dataset import SuperTuxClassificationDataset, get_class_names, load_data
+
 
