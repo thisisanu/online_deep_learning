71c1d912c3bcaa235d2cf9ae15f3639fe1338db4
thisisanu <168681048+thisisanu@users.noreply.github.com>
2025-12-08 22:23:29 -0800

    Import load_model from models in __init__.py

--- homework3/homework/__init__.py
+++ homework3/homework/__init__.py
@@ -1,3 +1,2 @@
 from .datasets.classification_dataset import SuperTuxClassificationDataset, get_class_names, load_data
-from .models import load_model  # <-- add this
-
+from .models import load_model
