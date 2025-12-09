9d22618af34faa54bb7b001238c23d6247a2b7cc
thisisanu <168681048+thisisanu@users.noreply.github.com>
2025-12-08 22:30:45 -0800

    Update __init__.py

--- homework3/homework/__init__.py
+++ homework3/homework/__init__.py
@@ -1,2 +1,2 @@
-from .datasets.classification_dataset import SuperTuxDataset, get_class_names, load_data
+from .datasets.classification_dataset import SuperTuxDataset, load_data
 from .models import load_model
